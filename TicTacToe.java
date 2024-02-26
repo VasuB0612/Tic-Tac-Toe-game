0cb688c9c942ba7d1d8e529d27a135e399fc0ae2
Vasu Bhatnagar <vasu.bhatnagar@tuni.fi>
2024-02-27 00:08:21 +0200

    entering the positions for the X and O.

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,14 +1,21 @@
+import java.util.Scanner;
+
 public class TicTacToe {
     public static void main(String[] args) {
         char [][] board = {
-                            {' ', '|', ' ', '|', ' '},
-                            {'-','+','-', '+','-'},
-                            {' ', '|', ' ', '|', ' '},
-                            {'-','+','-', '+','-'},
-                            {' ', '|', ' ', '|', ' '}
-                        };
+        {' ', '|', ' ', '|', ' '},
+        {'-', '+', '-', '+', '-'},
+        {' ', '|', ' ', '|', ' '},
+        {'-', '+', '-', '+', '-'},
+        {' ', '|', ' ', '|', ' '}
+    };
 
         gameBoard(board);
+
+        // Entering the positions of 'X' and 'O'
+
+        Scanner scan = new Scanner(System.in);
+        int position = scan.nextInt();
     }
 
     public static void gameBoard(char [][] board){
