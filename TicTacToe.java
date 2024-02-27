238d93ecf8e5cca2eedbe2308c77ef1a8ac1e878
Vasu Bhatnagar <vasu.bhatnagar@tuni.fi>
2024-02-27 21:01:31 +0200

    Created game for one player.

--- TicTacToe.java
+++ TicTacToe.java
@@ -14,8 +14,13 @@ public class TicTacToe {
 
         // Entering the positions of 'X' and 'O'
 
-        Scanner scan = new Scanner(System.in);
-        int position = scan.nextInt();
+        while(true){
+            Scanner scan1 = new Scanner(System.in);
+            System.out.println("Enter X in a position from 1 to 9: ");
+            int pos = scan1.nextInt();
+            moves(board, pos, 'X');
+            gameBoard(board);
+        }
     }
 
     public static void gameBoard(char [][] board){
@@ -24,6 +29,41 @@ public class TicTacToe {
                 System.out.print(c);
             }
             System.out.println();
+        }
+    }
+
+    public static void moves(char[][] board, int pos, char player){
+        switch (pos) {
+            case 1:
+                board[0][0] = player;
+                break;
+            case 2:
+                board[0][2] = player;
+                break;
+            case 3:
+                board[0][4] = player;
+                break;
+            case 4:
+                board[2][0] = player;
+                break;
+            case 5:
+                board[2][2] = player;
+                break;
+            case 6:
+                board[2][4] = player;
+                break;
+            case 7:
+                board[4][0] = player;
+                break;
+            case 8:
+                board[4][2] = player;
+                break;
+            case 9:
+                board[4][4] = player;
+                break;
+            default:
+                System.out.println("Invalid choice");
+                break;
         }
     }
 }
